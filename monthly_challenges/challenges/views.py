271f142ad2ba60072cfcd96ea79a79360bf3abcd
ciandial <tetris4@icloud.com>
2022-07-25 20:34:39 +0300

    More changes!

--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
+from django.http import Http404, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
+from django.template.loader import render_to_string
 
 monthly_challenges = {
     'january': "Eat no meat for the entire month!",
@@ -14,7 +15,7 @@ monthly_challenges = {
     'september': "Learn Django for at least 20 minutes every day!",
     'october': "Eat no meat for the entire month!",
     'november': "Walk for at least 20 minutes every day!",
-    'december': "Learn Django for at least 20 minutes every day!",
+    'december': None,
 }
 
 # Create your views here.
@@ -24,14 +25,9 @@ def index(request):
     list_items = ''
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('month-challenge', args=[month])
-        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-
-    response_data = f'<ul>{list_items}</ul>'
-
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {
+        'months': months
+    })
 
 
 def monthly_challenge_by_number(request, month):
@@ -46,10 +42,12 @@ def monthly_challenge_by_number(request, month):
     return HttpResponseRedirect(redirect_path)
 
 
-def monthly_challenge(request, month):
-    if month in monthly_challenges:
+def monthly_challenge(request, month, reaise=None):
+    try:
         challenge_text = monthly_challenges[month]
-        response_data = f"<h1>{challenge_text}</h1>"
-        return HttpResponse(response_data)
-    else:
-        return HttpResponseNotFound("<h1>This month is not supported!</h1>")
+        return render(request, 'challenges/challenge.html', {
+            'text': challenge_text,
+            'month_name': month
+        })
+    except:
+        raise Http404()
